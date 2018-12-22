edeb515fb44dfb540477d2f78848b620ac434f95
kristoferent <kristoferent@gmail.com>
2018-12-22 18:14:16 +0500

    mes s a ge

--- test.py
+++ test.py
@@ -7,6 +7,9 @@ for _ in range (n):
     x = random.randint(1, 100000)
     f.write(str(x))
     f.write(' ');
+f.write('\n')
+f.write(str(n))
+f.write('\n')
 for _ in range (n):
     x = random.randint(1, 100000)
     f.write(str(x))
